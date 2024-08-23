463dea2a67877a528399fe51dc9e29216b2ee089
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-08-23 08:35:15 +0530

    Create IS.cpp

---
+++ IS.cpp
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <math.h>
+
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+class Solution
+{
+     public:
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+        
+    }
+};
+
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
